2ffb1bca2bb963596228303095f9ec4ee5b70ac0
Nomynameisjames <adavaonimisi@gmail.com>
2022-12-15 01:16:43 +0100

    fixed bug in 0-create_a_file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,9 @@
-# Create a file
-file { '/tmp/holberton':
-  ensure  => 'file',
+#Using Puppet, create a file in /tmp.
+file { 'school':
+  ensure  => 'present',
+  path    => '/tmp/school',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet',
+  content => 'I love Puppet'
 }
